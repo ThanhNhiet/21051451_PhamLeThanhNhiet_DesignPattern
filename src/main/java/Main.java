6bdaff1a99172f11d96e2150187ac95b17f7a6b2
pham le thanh nhiet <nhiethiz@gmail.com>
2025-03-01 08:29:08 +0700

    done singleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,35 @@
 import singleton.EagerInitializedSingleton;
+import singleton.LazyInitializedSingleton;
+import singleton.StaticBlockSingleton;
+import singleton.ThreadSafeSingleton;
 
 public class Main {
     public static void main(String[] args) {
-        EagerInitializedSingleton singleton = EagerInitializedSingleton.getInstance();
-        EagerInitializedSingleton anotherSingleton = EagerInitializedSingleton.getInstance();
-        System.out.println(singleton.hashCode());
-        System.out.println(anotherSingleton.hashCode());
+//        EagerInitializedSingleton singleton = EagerInitializedSingleton.getInstance();
+//        EagerInitializedSingleton anotherSingleton = EagerInitializedSingleton.getInstance();
+//        System.out.println(singleton.hashCode());
+//        System.out.println(anotherSingleton.hashCode());
+
+//        StaticBlockSingleton singleton = StaticBlockSingleton.getInstance();
+//        StaticBlockSingleton anotherSingleton = StaticBlockSingleton.getInstance();
+//        System.out.println(singleton.hashCode());
+//        System.out.println(anotherSingleton.hashCode());
+
+        //Dung nhieu
+//        LazyInitializedSingleton singleton = LazyInitializedSingleton.getInstance();
+//        LazyInitializedSingleton anotherSingleton = LazyInitializedSingleton.getInstance();
+//        System.out.println(singleton.hashCode());
+//        System.out.println(anotherSingleton.hashCode());
+
+
+        new Thread(() -> {
+            ThreadSafeSingleton singleton = ThreadSafeSingleton.getInstance();
+            System.out.println(singleton.hashCode());
+        }).start();
+        new Thread(() -> {
+            ThreadSafeSingleton singleton = ThreadSafeSingleton.getInstance();
+            System.out.println(singleton.hashCode());
+        }).start();
 
     }
 }
